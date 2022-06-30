4a99a5c2be09492ed45a9fdda16aa87f64e03ef1
vishnu244 <vishnunali244@gmail.com>
2022-06-30 13:31:35 +0530

    [Vishnu]-Using Json Object get the details of Employees

--- EmployeepayRoll.js
+++ EmployeepayRoll.js
@@ -38,16 +38,16 @@ const createInnerHtml = () => {
 
 
 
-
+//UC-4
 //using template literal ES6 Feature
-
+/*
 window.addEventListener('DOMContentLoaded', (Event) =>{
     createInnerHtml();
 });
 
 const createInnerHtml = () => {
     const headerHtml = "<th>Profile</th><th><pre >Name</pre></th><th>Gender</th><th>Department</th><th>Salary</th><th>startDate</th><th>Action</th>";              
-                                        
+                                         
     const innerHtml = `${headerHtml}
                        
                 <tr>
@@ -70,4 +70,54 @@ const createInnerHtml = () => {
                 
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
+}*/
+
+//UC5
+
+window.addEventListener('DOMContentLoaded', (Event) =>{
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml = "<th>Profile</th><th><pre >Name</pre></th><th>Gender</th><th>Department</th><th>Salary</th><th>startDate</th><th>Action</th>";              
+    let empPayrollData = createEmployeePayrollJSON()[0];                                   
+    const innerHtml = `${headerHtml}
+                       
+                <tr>
+                    <td><img class="profile" alt="" src="${empPayrollData._profile}"></td>
+                    <td>${empPayrollData._name} </td>
+                    <td>${empPayrollData._Gender}</td>
+                    <td><div class='dept-label'>${empPayrollData._department[0]}</div>
+                        <div class='dept-label'>${empPayrollData._department[1]}</div>
+                        <div class='dept-label'>${empPayrollData._department[2]}</div></td>
+                 
+                    <td>${empPayrollData._Salary}</td>
+                    <td>${empPayrollData._StartDate}</td>
+                    <td>
+                        <img name = "${empPayrollData._id}" class = "Icon delete" onclick="remove(this)" alt="delete"
+                            src="image/dd.jpg">
+                        <img name = "${empPayrollData._id}" class = "Icon" alt="edit" onclick="update(this)" alt="edit"
+                            src="image/edit-Icon.png">
+                    </td>         
+                </tr>
+                
+    `;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+const createEmployeePayrollJSON =() => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Vishnu Vardhan',
+            _Gender:'Male',
+            _department: [
+                'Sales','HR','Finance'
+            ],
+            _Salary: '30000',
+            _StartDate: '10-04-2022',
+            _note:'',
+            _id: new Date.getTime(),
+            _profile: 'image/profile2.jpg'
+        }
+    ];
+    return empPayrollListLocal;
 }
